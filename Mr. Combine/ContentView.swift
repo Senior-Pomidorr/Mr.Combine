3cd8a01b2415fb82f41b2fc84122cd3e59a8193a
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2024-02-25 19:09:59 +0300

    allSatisfy

--- Mr. Combine/ContentView.swift
+++ Mr. Combine/ContentView.swift
@@ -9,32 +9,42 @@ import SwiftUI
 import Combine
 
 struct ContentView: View {
-    @StateObject private var vm = DataTaskPublisher()
-    @State private var age = ""
+    @State private var number = ""
+    @State private var checkNumbers = false
+    @StateObject private var vm = AllSatisfy()
     var body: some View {
         VStack(spacing: 20) {
-            HeaderView("TimeInterval",
+            HeaderView("AllSatisfy",
                        subtitle: "Introduction",
-                       desc: "The Time Interval")
+                       desc: "AllSatisfy")
             .layoutPriority(1)
             
-            vm.dataToView?
-                .resizable()
-                .scaledToFit()
-                
-    
+            HStack {
+                TextField("add a number", text: $number)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.numberPad)
+                Button {
+                    vm.add(number: number)
+                    number = ""
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .padding()
+            
+            List(vm.numbers, id: \.self) { number in
+                Text("\(number)")
+            }
+            Spacer(minLength: 0)
+            Button("Fibanachi Numbers") {
+                vm.allFibanachiCheck()
+                checkNumbers = true
+            }
+            Text(vm.allFibanachiNumbers ? "Yes" : "No")
+                .opacity(checkNumbers ? 1 : 0)
         }
-        .onAppear() {
-            vm.fetch()
-        }
-        .alert(item: $vm.errorForAlert) { errorForAlert in
-            Alert(
-                title: Text(
-                    errorForAlert.title
-                ),
-                message: Text(errorForAlert.message)
-            )
-        }
+        .padding(.bottom)
+        .font(.title)
     }
 }
 
--- Mr. Combine/Model.swift
+++ Mr. Combine/Model.swift
@@ -8,44 +8,24 @@
 import SwiftUI
 import Combine
 
-struct ErrorForAlert: Error, Identifiable {
-    let id = UUID()
-    var title = ""
-    var message: String
-    
-    init(message: String) {
-        self.message = message
-    }
-}
 
-
-
-final class DataTaskPublisher: ObservableObject {
-    @Published var dataToView: Image?
-    @Published var errorForAlert: ErrorForAlert?
+final class AllSatisfy: ObservableObject {
+    @Published var numbers = [Int]()
+    @Published var allFibanachiNumbers = false
     var cancellbales: Set<AnyCancellable> = []
     
-    func fetch() {
-        let url = URL(string: "https://images.unsplash.com/photo-1708721205486-dd5afb8cd39e?q=80&w=2663&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")!
-        URLSession.shared.dataTaskPublisher(for: url)
-            .map{ (data: Data, response: URLResponse) in
-                data
+    func allFibanachiCheck() {
+        let fibanachiNumbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+        numbers.publisher
+            .allSatisfy { number in
+                fibanachiNumbers.contains(number)
             }
-            .tryMap({ data in
-                guard let image = UIImage(data: data) else { throw
-                    ErrorForAlert(message: "Did not receive a valid image.")
-                }
-                return Image(uiImage: image)
-            })
-            .receive(on: RunLoop.main)
-            .sink(receiveCompletion: { [unowned self] completion in
-                if case .failure(let error) = completion {
-                    errorForAlert = ErrorForAlert(message: "Details: \(error.localizedDescription)")
-                }
-            }, receiveValue: { [unowned self] image in
-                dataToView = image
-            })
-            .store(in: &cancellbales)
+            .assign(to: &$allFibanachiNumbers)
+    }
+    
+    func add(number: String) {
+        if number.isEmpty { return }
+        numbers.append(Int(number) ?? 0)
     }
     
 }
